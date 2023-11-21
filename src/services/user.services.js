fccd1688eaadd72b62222a2fdb7fad85b7ab68a4
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-21 06:25:31 -0500

    Create User Login

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -2,10 +2,10 @@ import { prisma } from "../prisma/index.js";
 import { bcrypt } from "../utils/bcrypt.js";
 import { crypto } from "../utils/crypto.js";
 import { mailer } from "../utils/mailer.js";
-// import { date } from "../utils/date.js";
-// import jwt from "jsonwebtoken";
-// import { v4 as uuid } from "uuid";
-// import { CustomError } from "../utils/custom-error.js";
+import { date } from "../utils/date.js";
+import jwt from "jsonwebtoken";
+import { v4 as uuid } from "uuid";
+import { CustomError } from "../utils/custom-error.js";
 
 class UserService {
     signUp = async (userInput) => {
@@ -24,6 +24,62 @@ class UserService {
         });
 
         await mailer.sendActivationMail(userInput.email, activationToken);
+    };
+
+    login = async (input) => {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: input.email
+            },
+            select: {
+                id: true,
+                status: true,
+                password: true
+            }
+        });
+
+        if (!user) throw new CustomError("User does not exist", 404);
+
+        if (user.status === "INACTIVE") {
+            const activationToken = crypto.createToken();
+            const hashedActivationToken = crypto.hash(activationToken);
+
+            await prisma.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    activationToken: hashedActivationToken
+                }
+            });
+
+            await mailer.sendActivationMail(input.email, activationToken);
+
+            throw new CustomError(
+                "We just sent you activation email. Follow instructions",
+                400
+            );
+        }
+
+        const isPasswordMatches = await bcrypt.compare(
+            input.password,
+            user.password
+        );
+        if (!isPasswordMatches) {
+            throw new CustomError("Invalid Credentials", 401);
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "2 days"
+            }
+        );
+
+        return token;
     };
 }
 
