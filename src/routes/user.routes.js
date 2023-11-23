41461b3551f6a0bf4bf463dc3d4afb9a5272ff15
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-23 00:01:45 -0500

    Implement Get Me

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-// import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { userController } from "../controllers/user.controller.js";
 
 const userRouter = Router();
@@ -9,5 +9,6 @@ userRouter.get("/activate", userController.activate);
 userRouter.post("/login", userController.login);
 userRouter.patch("/forgot-password", userController.forgotPassword);
 userRouter.patch("/reset-password", userController.resetPassword);
+userRouter.get("/me", authMiddleware.authenticate, userController.getMe);
 
 export { userRouter };
